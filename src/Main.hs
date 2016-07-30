45986a5b515a74ec3de454b6ea3ea71aa7d4ef10
kolen <incredible.angst@gmail.com>
2016-07-30 17:21:17 +0300

    Move reading to instances

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Data.Sequence
@@ -19,6 +21,36 @@ data BSPHeader = BSPHeader
 
 data Vertex = Vertex Float Float Float
   deriving (Show)
+
+data Edge = Edge Word16 Word16
+
+data BSPMap = BSPMap
+  { vertices :: Seq Vertex,
+    edges :: Seq Edge}
+
+class LumpData a where
+  lumpIndex :: a -> Int
+  readLumpData :: LumpEntry -> Get a
+  readLumpFromFile :: Handle -> Seq LumpEntry -> IO a
+  readLumpFromFile fh entries = do
+    bytes <- readLumpBytes fh entry
+    return $ runGet (readLumpData entry) bytes
+      where entry = entries `index` (lumpIndex (undefined :: a))
+
+instance LumpData (Seq Vertex) where
+  readLumpData lump =
+    readArray lump $ Vertex <$> getFloatle <*> getFloatle <*> getFloatle
+  lumpIndex = const 2
+
+instance LumpData (Seq Edge) where
+  readLumpData lump =
+    readArray lump $ Edge <$> getWord16le <*> getWord16le
+  lumpIndex = const 6
+
+readLumpBytes :: Handle -> LumpEntry -> IO BL.ByteString
+readLumpBytes handle (LumpEntry offset length) = do
+  hSeek handle AbsoluteSeek (fromIntegral offset)
+  BL.hGet handle (fromIntegral length)
 
 readLumpEntry :: Get LumpEntry
 readLumpEntry = LumpEntry <$> getWord32le <*> getWord32le
@@ -55,24 +87,19 @@ readArray lumpEntry readItem = do
             rest <- readArray lumpEntry readItem
             return $ record <| rest
 
-readVertices :: LumpEntry -> Get (Seq Vertex)
-readVertices lump =
-  readArray lump $ Vertex <$> getFloatle <*> getFloatle <*> getFloatle
-
-readLumpContents :: Handle -> LumpEntry -> IO BL.ByteString
-readLumpContents handle (LumpEntry offset length) = do
-  hSeek handle AbsoluteSeek (fromIntegral offset)
-  BL.hGet handle (fromIntegral length)
-
 main :: IO ()
 main = do
-  args <- getArgs
-  let filename = head args
-  fh <- openFile filename ReadMode
-  headerData <- BL.hGetContents fh
-  let header = runGet readBspHeader headerData
-  let verticesLumpEntry = lumpEntries header `index` 2
-  verticesLumpContents <- readLumpContents fh verticesLumpEntry
-  let vertices = runGet (readVertices verticesLumpEntry) verticesLumpContents
+  -- args <- getArgs
+  -- let filename = head args
+  -- fh <- openFile filename ReadMode
+  -- headerData <- BL.hGetContents fh
+  -- let header = runGet readBspHeader headerData
+  -- let verticesLumpEntry = lumpEntries header `index` 2
+  -- verticesLumpContents <- readLumpContents fh verticesLumpEntry
+  -- let vertices = runGet (readVertices verticesLumpEntry) verticesLumpContents
+  -- let edgesLumpEntry = lumpEntries header `index` 11
+  -- edgesLumpContents <- readLumpContents fh edgesLumpEntry
+  -- let edges = runGet (readEdges edgesLumpEntry) edgesLumpContents
 
-  print $ Data.Sequence.take 100 vertices
+  -- print $ Data.Sequence.take 100 vertices
+  print ""
